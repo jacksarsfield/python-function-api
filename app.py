57de20ab785c7f0c4797e6cb0b0bbb6e6a6cdb8f
John Stephens <mr_john_stephens@hotmail.com>
2017-03-29 09:40:13 +0100

    added in function exec again

--- app.py
+++ app.py
@@ -7,12 +7,11 @@ app = Flask(__name__)
 def main():
     return "Welcome!"
 
-#@app.route('/api/function/<name>/exec', methods = ['GET','POST'])
-#def execute(name):
-#    input = request.data
-#    output = getattr(function, name)(input)
-#    return output
-#    return name;
+@app.route('/api/function/<name>/exec', methods = ['GET','POST'])
+def execute(name):
+    input = request.data
+    output = getattr(function, name)(input)
+    return output
         
 if __name__ == '__main__':
    app.run()
